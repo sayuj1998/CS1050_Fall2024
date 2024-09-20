cd8ba0f570e9ab5c62661bba18cf759cfe587aeb
sayuj1998 <sayuj21@gmail.com>
2024-09-19 21:20:19 -0600

    Updated

--- CS1050MyAssign/ShresthaSayujGE02.java
+++ CS1050MyAssign/ShresthaSayujGE02.java
@@ -3,7 +3,7 @@
  * Class: CS1050 (T/TH)
  * Due Date: 09/21/2024
  * Assignment #2
- * This program will print two students initials in order.
+ * This program will print two students' initials in order.
  */
 
 import java.util.Scanner;
@@ -14,14 +14,37 @@ public class ShresthaSayujGE02 {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Student 1 first name: ");
-        System.out.println("Enter Student 2 last name: ");
-        System.out.println("Enter Student 1 first name: ");
-        System.out.println("Enter Student 2 last name: ");
+        System.out.print("Enter Student 1 first name: ");
+        String student1FirstName = input.next();
 
+        System.out.print("Enter Student 1 last name: ");
+        String student1LastName = input.next();
 
+        System.out.print("Enter Student 2 first name: ");
+        String student2FirstName = input.next();
 
+        System.out.print("Enter Student 2 last name: ");
+        String student2LastName = input.next();
 
+        // Gets the first initials of both students
+        char initialStudent1FirstName = student1FirstName.charAt(0);
+        char initialStudent1LastName = student1LastName.charAt(0);
+        char initialStudent2FirstName = student2FirstName.charAt(0);
+        char initialStudent2LastName = student2LastName.charAt(0);
+
+        // Compares the last name initials for ordering
+        if (initialStudent1LastName > initialStudent2LastName)
+        {
+            System.out.printf("Student 1: %s.%s.\n", initialStudent2FirstName, initialStudent2LastName);
+            System.out.printf("Student 2: %s.%s.\n", initialStudent1FirstName, initialStudent1LastName);
+        }
+        else
+        {
+            System.out.printf("Student 1: %s.%s.\n", initialStudent1FirstName, initialStudent1LastName);
+            System.out.printf("Student 2: %s.%s.\n", initialStudent2FirstName, initialStudent2LastName);
+        }
+
+        input.close();
 
     } // end of main
 } // end of class
--- PersonalProjects/src/Example.java
+++ PersonalProjects/src/Example.java
@@ -13,6 +13,7 @@ public class Example
             case 1: System.out.println("Monday"); break;
             case 2: System.out.println("Tuesday"); break;
             case 3: System.out.println("Wednesday"); break;
+
         }
 
 
